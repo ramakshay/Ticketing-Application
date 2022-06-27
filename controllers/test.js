70130be8fc2c94498b57b6d06f3b6bcc172a0215
ramakshay <63576214+ramakshay@users.noreply.github.com>
2022-06-27 21:00:22 +0530

    Delete controllers directory

--- controllers/test.js
+++
@@ -1,8 +0,0 @@
-exports.testRo = function (req,res,next){
-    res.json({
-        test : {
-            title : 'classified information',
-            data : 'This is visible only to authenticated users'
-        }
-    });
-}
