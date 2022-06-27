a6480c9c52aeda576116b68e1f29dba3aefa0d19
ramakshay <63576214+ramakshay@users.noreply.github.com>
2022-06-27 21:01:23 +0530

    Delete routes directory

--- routes/testingRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const verify = require('../middleware/verifyAdmin');
-
-const getTest = require('../controllers/test');
-
-router.get("/", verify ,getTest.testRo);
-
-module.exports = router;
